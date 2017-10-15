f9a0b53f9188cf8133e982762af6906c3faa7f1e
Devendra Dora <dev.tech24@gmail.com>
2017-10-15 23:16:27 +0530

    added readme

--- Math.java
+++ Math.java
@@ -8,7 +8,8 @@ public static double mul(double num1,double num2){
 
 }
 	public static void main(String[] args) {
+		System.out.println("Math Test");
 		System.out.println("sum "+sum(2,3));
-		System.out.println("mul"+mul(5,6));
+		
 	}
 }
